b155a219adf6e297d2bb4343bc1a694a9b4171f6
Ayansheikh1 <ayansheikh0698@gmail.com>
2024-08-06 00:39:14 +0530

    arrange properly

--- data/cart.js
+++ data/cart.js
@@ -1,6 +1,6 @@
 export let cart = JSON.parse(localStorage.getItem('cart'));
 
-if(!cart){
+if (!cart) {
  cart =  [{
     productId: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
    quantity: 2,
@@ -10,6 +10,11 @@ if(!cart){
     quantity: 1,
    }
   ];
+}
+
+
+function saveStorage(){
+  localStorage.setItem('cart', JSON.stringify(cart));
 }
 
 
@@ -58,7 +63,3 @@ saveStorage();
 
 }
 
-
-function saveStorage(){
-  localStorage.setItem('cart', JSON.stringify(cart));
-}
