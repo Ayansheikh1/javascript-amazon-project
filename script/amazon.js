a580621f80184eedd316fb935f3edc3c0786448e
Ayansheikh1 <ayansheikh0698@gmail.com>
2024-08-10 23:46:16 +0530

    make separate function for delivery option

--- script/amazon.js
+++ script/amazon.js
@@ -4,7 +4,6 @@ import { formatCurrency } from './utils/money.js';
 
 
 
-let productsHTML = '';
 
 
 products.forEach((product) => {
--- script/checkout.js
+++ script/checkout.js
@@ -1,6 +1,11 @@
 import {cart, removeFromCart, calculateCartQuantity, updateQuantity} from '../data/cart.js';
 import {products} from '../data/products.js';
 import { formatCurrency } from './utils/money.js';
+import dayjs from 'https://unpkg.com/dayjs@1.11.10/esm/index.js';
+
+
+
+
 
 
 let cartItemHtml;
