1e2ee6a716890b6ca066f109d74356941a45db14
Ayansheikh1 <ayansheikh0698@gmail.com>
2024-08-19 00:27:20 +0530

    separate getproduct into function and save the data of productprice according to price and quantity

--- script/checkout.js
+++ script/checkout.js
@@ -1,8 +1,12 @@
 import{renderOrderSummary} from './checkout/orderSummary.js';
+import { renderPaymentSummary } from './checkout/paymentSummary.js';
 
 
 
 renderOrderSummary();
+
+renderPaymentSummary();
+
 
 
 
