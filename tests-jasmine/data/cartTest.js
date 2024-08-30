216b51cc25ada14aff2b21705f5a4e78ba7d3d84
Ayansheikh1 <ayansheikh0698@gmail.com>
2024-08-30 22:42:58 +0530

    work on cartTest

--- tests-jasmine/data/cartTest.js
+++ tests-jasmine/data/cartTest.js
@@ -33,18 +33,14 @@ describe('test suite: addToCart', () =>{
       return JSON.stringify([]);
     });
 
-  
-
     loadFromStorage();
     
-    
-  
-
     addToCart('e43638ce-6aa0-4b85-b27f-e1d07eb678c6');
     expect(cart.length).toEqual(1);
     expect(localStorage.setItem).toHaveBeenCalledTimes(1);
     expect(cart[0].productId).toEqual('e43638ce-6aa0-4b85-b27f-e1d07eb678c6');
     expect(cart[0].quantity).toEqual(1);
+    
     
   });
 });
