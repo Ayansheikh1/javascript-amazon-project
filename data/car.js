bbed5c9196e1f50184d909b8d7b3aa0eb05753d2
Ayansheikh1 <ayansheikh0698@gmail.com>
2024-09-09 20:49:27 +0530

    add trunk property and make brand and model private property

--- data/car.js
+++ data/car.js
@@ -1,25 +1,33 @@
 class Car{
-  brand;
-  model;
+  #brand;
+  #model;
   speed = 0;
+  isTrunkOpen = false;
 
   constructor(carDetails){
-    this.brand = carDetails.brand;
-    this.model = carDetails.model;
+    this.#brand = carDetails.brand;
+    this.#model = carDetails.model;
   }
 
   displayInfo(){
+    const trunkStatus = this.isTrunkOpen ? 'open' : 'closed';
 
-  console.log(`${this.brand} ${this.model} , Speed: ${this.speed} km/h`);
+  console.log(`${this.#brand} ${this.#model} , Speed: ${this.speed} km/h , Trunk: ${trunkStatus}` );
 
   }
 
   go(){
-    this.speed = this.speed + 5;
+
+    if(!this.isTrunkOpen){
+      this.speed = this.speed + 5;
+    }
+    
 
     if(this.speed > 200){
       this.speed = 200;
     }
+
+    
   }
 
   brake(){
@@ -29,6 +37,46 @@ class Car{
       this.speed = 0;
     }
 
+  }
+
+  openTrunk(){
+    if(this.speed === 0){
+      this.isTrunkOpen = true;
+    }
+  }
+
+  closeTrunk(){
+   
+    this.isTrunkOpen = false;
+    
+  }
+
+}
+
+
+class RaceCar extends Car{
+
+  acceleration;
+
+  constructor(carDetails){
+    super(carDetails);
+    this.acceleration = carDetails.acceleration;
+
+  }
+
+  go(){
+    this.speed += this.acceleration;
+    if(this.speed > 300){
+      this.speed = 300;
+    }
+  }
+
+  openTrunk(){
+    console.log('Race car do not have trunk');
+  }
+
+  closeTrunk(){
+    console.log('Race car do not have trunk');
   }
 }
 
@@ -42,15 +90,20 @@ const car2 = new Car({
   model: 'Model 3'
 });
 
+const raceCar = new RaceCar({
+  brand: 'McLearn',
+  model: 'F1',
+  acceleration: 20
+});
+
 console.log(car1);
 
 console.log(car2);
 
-car1.displayInfo();
-car1.go();
-car1.go();
-car1.go();
-car1.brake();
+console.log(raceCar);
+
+
+car1.openTrunk();
 car1.displayInfo();
 
 car2.displayInfo();
@@ -58,3 +111,21 @@ car2.go();
 car2.brake();
 car2.brake();
 car2.displayInfo();
+
+// Trunk should open since the car is not moving.
+car2.openTrunk();
+// Car should not go since the trunk is open.
+car2.go();
+car2.displayInfo();
+
+
+raceCar.go();
+raceCar.go();
+raceCar.go();
+raceCar.displayInfo();
+raceCar.openTrunk();
+raceCar.displayInfo();
+raceCar.brake();
+raceCar.displayInfo();
+
+
