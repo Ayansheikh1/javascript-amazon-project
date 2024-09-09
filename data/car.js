465ad5de127e712020e1249b7587f4b205a9afe4
Ayansheikh1 <ayansheikh0698@gmail.com>
2024-09-09 19:38:06 +0530

    add speed property

--- data/car.js
+++ data/car.js
@@ -1,6 +1,7 @@
 class Car{
   brand;
   model;
+  speed = 0;
 
   constructor(carDetails){
     this.brand = carDetails.brand;
@@ -8,10 +9,27 @@ class Car{
   }
 
   displayInfo(){
-  console.log(`${this.brand} ${this.model}`);
-    
+
+  console.log(`${this.brand} ${this.model} , Speed: ${this.speed} km/h`);
+
   }
 
+  go(){
+    this.speed = this.speed + 5;
+
+    if(this.speed > 200){
+      this.speed = 200;
+    }
+  }
+
+  brake(){
+    this.speed = this.speed - 5;
+
+    if(this.speed < 0){
+      this.speed = 0;
+    }
+
+  }
 }
 
 const car1 = new Car({
@@ -29,4 +47,14 @@ console.log(car1);
 console.log(car2);
 
 car1.displayInfo();
+car1.go();
+car1.go();
+car1.go();
+car1.brake();
+car1.displayInfo();
+
+car2.displayInfo();
+car2.go();
+car2.brake();
+car2.brake();
 car2.displayInfo();
